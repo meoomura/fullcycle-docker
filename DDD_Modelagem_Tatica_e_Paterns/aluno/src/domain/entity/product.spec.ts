95235c02ba8f94cd5096d05723c5cf984d57aa90
meoomura <marcos.omura@gmail.com>
2023-09-13 17:03:55 -0300

    DDD - Desafio de repositório

---
+++ DDD_Modelagem_Tatica_e_Paterns/aluno/src/domain/entity/product.spec.ts
@@ -0,0 +1,36 @@
+import Product from "./product";
+
+describe("product unit tests", () => {
+    
+    it("should throw error when id is empty", () => {
+        expect( () => {
+            let product = new Product("","Product 1", 100);
+        }).toThrowError("Id is required");
+    });
+
+    it("should throw error when name is empty", () => {
+        expect( () => {
+            let product = new Product("P1","", 100);
+        }).toThrowError("Name is required");
+    });
+
+    it("should throw error when price is less than 0", () => {
+        expect( () => {
+            let product = new Product("P1","Product 1", -1);
+        }).toThrowError("Price must be greater than 0");
+    });
+
+    it("should change name", () => {
+        const product = new Product("P1","Produto 1", 100);
+        product.changeName("Produto X");
+        expect(product.name).toBe("Produto X");
+    });
+
+    it("should change price", () => {
+        const product = new Product("P1","Produto 1", 100);
+        product.changePrice(200);
+        expect(product.price).toBe(200);
+    });
+
+
+});
--- README.md
+++ README.md
@@ -1,7 +1,10 @@
-# Desafios Curso Full Cycle Docker
+# Desafios Curso Full Cycle 3
 
 ## Desafio Go
 Pasta docker/desafioGo
 
 ## Desafio Nginx com Node.js
 Pasta docker/nginxComNodeJs
+
+## DDD: Modelagem Tática e Patterns Desafio de repositório
+Pasta DDD_Modelagem_Tatica_e_Paterns/aluno
