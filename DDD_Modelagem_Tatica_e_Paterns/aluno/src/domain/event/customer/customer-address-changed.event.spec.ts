4d4633afb6020bb4ad50dab71fe9aae644d92d01
meoomura <marcos.omura@gmail.com>
2023-09-14 12:11:51 -0300

    Desafio Domain Events

---
+++ DDD_Modelagem_Tatica_e_Paterns/aluno/src/domain/event/customer/customer-address-changed.event.spec.ts
@@ -0,0 +1,30 @@
+import Address from "../../entity/address";
+import Customer from "../../entity/customer";
+import EventDispatcher from "../@shared/event-dispatcher";
+import CustomerAddressChangedEvent from "./customer-address-changed.event";
+import SendMessageWhenCustomerAddressIsChangedHandler from "./handler/send-message-when-customer-address-is-changed.handler";
+
+describe("Customer address changed events tests", () => {
+    it("should notify all event handlers", () => {
+        const eventDispatcher = new EventDispatcher();
+        const eventHandler = new SendMessageWhenCustomerAddressIsChangedHandler();
+        const spyEventHandler = jest.spyOn(eventHandler, "handle");
+
+        const customer = new Customer("123", "John");
+  
+        const address =  new Address("Rua sem nome", 10, "12345-678", "São Paulo", "SP");
+        customer.changeAddress(address);
+
+        eventDispatcher.register("CustomerAddressChangedEvent",eventHandler);
+        
+        expect(eventDispatcher.getEventHandlers["CustomerAddressChangedEvent"][0]).toBe(eventHandler);
+
+        const customerAddressChangedEvent =  new CustomerAddressChangedEvent(customer);
+        
+        eventDispatcher.notify(customerAddressChangedEvent);
+
+        expect(spyEventHandler).toHaveBeenCalled();
+        
+    });
+
+})
