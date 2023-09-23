08cdfce2874f4d9448f800627be8d1e1cc83a865
meoomura <marcos.omura@gmail.com>
2023-09-23 10:38:54 -0300

    Desafio Notification Pattern em Products

--- clean_architecture/src/domain/product/entity/product.spec.ts
+++ clean_architecture/src/domain/product/entity/product.spec.ts
@@ -5,25 +5,39 @@ describe("product unit tests", () => {
     it("should throw error when id is empty", () => {
         expect( () => {
             let product = new Product("","Product 1", 100);
-        }).toThrowError(new Error("Id is required"));
+        }).toThrowError(new Error("product: Id is required"));
     });
 
     it("should throw error when name is empty", () => {
         expect( () => {
             let product = new Product("P1","", 100);
-        }).toThrowError(new Error("Name is required"));
+        }).toThrowError(new Error("product: Name is required"));
     });
 
     it("should throw error when price is less than 0", () => {
         expect( () => {
             let product = new Product("P1","Product 1", -1);
-        }).toThrowError(new Error("Price must be greater than 0"));
+        }).toThrowError(new Error("product: Price must be greater than 0"));
     });
+
+    it("should throw error when id and name are empty and price is less than 0", () => {
+        expect( () => {
+            let product = new Product("","", -1);
+        }).toThrowError(new Error("product: Id is required,product: Name is required,product: Price must be greater than 0"));
+    });
+
 
     it("should change name", () => {
         const product = new Product("P1","Produto 1", 100);
         product.changeName("Produto X");
         expect(product.name).toBe("Produto X");
+    });
+
+    it("should throw error when change to name empty", () => {
+        expect( () => {
+            const product = new Product("P1","Produto 1", 100);
+            product.changeName("");
+            }).toThrowError(new Error("product: Name is required"));
     });
 
     it("should change price", () => {
@@ -32,5 +46,11 @@ describe("product unit tests", () => {
         expect(product.price).toBe(200);
     });
 
+    it("should throw error when change price to value less than 0", () => {
+        expect( () => {
+            const product = new Product("P1","Produto 1", 100);
+            product.changePrice(-10);
+        }).toThrowError(new Error("product: Price must be greater than 0"));
+    });
 
 });
