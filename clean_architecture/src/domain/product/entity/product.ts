16fdbafb6638e1fbbede526954ef82cb55cb1764
meoomura <marcos.omura@gmail.com>
2023-09-23 11:46:51 -0300

    desafio validacao produtos

--- clean_architecture/src/domain/product/entity/product.ts
+++ clean_architecture/src/domain/product/entity/product.ts
@@ -1,5 +1,6 @@
 import Entity from "../../@shared/entity/entity.abstract";
 import NotificationError from "../../@shared/notification/notification.error";
+import ProductValidatorFactory from "../factory/product.validator.factory";
 
 export default class Product extends Entity{
 
@@ -33,29 +34,12 @@ export default class Product extends Entity{
     }
 
     validate() : boolean{
-        if(this._id.length === 0){
-            this.notification.addError({
-                message: "Id is required",
-                context: "product"
-            });
-        }
-        if(this._name.length === 0){
-            this.notification.addError({
-                message: "Name is required",
-                context: "product"
-            });
-        }
-        if(this._price < 0){
-            this.notification.addError({
-                message: "Price must be greater than 0",
-                context: "product"
-            });
-        }
+        ProductValidatorFactory.create().validate(this);
 
         if(this.notification.hasErrors()){
             throw new NotificationError(this.notification.getErrors());
         }
-       
+        
         return true;
     }
 
